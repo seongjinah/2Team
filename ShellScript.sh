9fb613e06a6488e081a3f3eb5128bc5f989a2922
Gyu-won <54783290+Gyu-won@users.noreply.github.com>
2020-06-28 16:32:34 +0900

    Update ShellScript.sh

--- ShellScript.sh
+++ ShellScript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+$HOME/elasticsearch-7.6.2/bin/elasticsearch -d
+
 pip install numpy
 pip install django
 pip install nltk
@@ -12,24 +14,25 @@ if [ ! -d static ]; then
     mkdir -p static/css
 fi
 
-cp -f 1.html templates
-cp -f 2-1.html templates
-cp -f 2-2.html templates
-cp -f 3-1.html templates
-cp -f 3-2.html templates
-cp -f 4-1.html templates
-cp -f 4-2.html templates
-cp -f 7.html templates
+cp -f $HOME/2Team/1.html templates
+cp -f $HOME/2Team/2-1.html templates
+cp -f $HOME/2Team/2-2.html templates
+cp -f $HOME/2Team/3-1.html templates
+cp -f $HOME/2Team/3-2.html templates
+cp -f $HOME/2Team/4-1.html templates
+cp -f $HOME/2Team/4-2.html templates
+cp -f $HOME/2Team/7.html templates
 
-cp -f 1.css static/css
-cp -f 2-1.css static/css
-cp -f 2-2.css static/css
-cp -f 3-1.css static/css
-cp -f 3-2.css static/css
-cp -f 4-1.css static/css
-cp -f 4-2.css static/css
-cp -f 7.css static/css
+cp -f $HOME/2Team/1.css static/css
+cp -f $HOME/2Team/2-1.css static/css
+cp -f $HOME/2Team/2-2.css static/css
+cp -f $HOME/2Team/3-1.css static/css
+cp -f $HOME/2Team/3-2.css static/css
+cp -f $HOME/2Team/4-1.css static/css
+cp -f $HOME/2Team/4-2.css static/css
+cp -f $HOME/2Team/7.css static/css
 
-$HOME/elasticsearch-7.6.2/bin/elasticsearch -d
+cp -f $HOME/2Team/app.py $HOME
+cp -f $HOME/2Team/textfile.txt $HOME
 
 flask run 
